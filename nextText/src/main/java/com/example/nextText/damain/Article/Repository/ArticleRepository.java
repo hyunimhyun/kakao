f4ae096eeca7e1230d313e7a8db7477503a3e947
hyunimhyun <125636349+hyunimhyun@users.noreply.github.com>
2025-06-18 12:25:11 +0900

    Delete nextText directory

--- nextText/src/main/java/com/example/nextText/damain/Article/Repository/ArticleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.nextText.damain.Article.Repository;
-
-
-import com.example.nextText.damain.Article.Entity.Article;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ArticleRepository extends JpaRepository<Article, Long> {
-}
